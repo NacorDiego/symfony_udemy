b781b62589865bd51b8dd8cc0e7df5f58e55c3b6
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-08-08 21:41:59 -0300

    TemplateController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,9 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home_inicio')]
-    public function maria(): Response
+    #[Route('/', name: 'home_inicio')]
+    public function Home(): Response
     {
         die("Hola desde el controlador HomeController");
+    }
+
+    #[Route('/home/saludo', name: 'home_saludo')]
+    public function saludo(): Response
+    {
+        die("hola te saludo");
+    }
+
+    #[Route('/home/despedida', name: 'home_despedida')]
+    public function despedida(): Response
+    {
+        die("hola me despido");
     }
 }
