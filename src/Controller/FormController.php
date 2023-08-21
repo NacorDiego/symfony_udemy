a9d872663e67132896de51f8a409f8bea63df14d
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-08-21 12:11:59 -0300

    Creo 'formulario_simple' mediante helper de symfony en el controlador. Implemento una plantilla de bootstrap para los formularios en 'twig.yaml'. Inicio creación de formulario personalizado en el mismo controlador 'FormController'

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class FormController extends AbstractController
 {
@@ -12,5 +16,35 @@ class FormController extends AbstractController
     public function index(): Response
     {
         return $this->render('form/index.html.twig');
+    }
+    #[Route('/form/simple', name: 'form_simple')]
+    public function simple(): Response
+    {
+        // Creo un formulario mediante creaFormBuilder.
+        $formulario = $this->createFormBuilder(null)
+                    ->add('nombre',TextType::class, ['label'=>'Nombre'])
+                    ->add('imagen',TextType::class,['label'=>'Imagen'])
+                    ->add('descripcion',TextareaType::class,['label'=>'Descripción'])
+                    ->add('plataforma')
+                    ->add('genero')
+                    ->add('url',UrlType::class,['label'=>'Sitio web'])
+                    ->add('addGame',SubmitType::class)
+                    ->getForm();
+        return $this->render('form/simple.html.twig', compact('formulario'));
+    }
+    #[Route('/form/bootstrap', name: 'form_bootstrap')]
+    public function bootstrap(): Response
+    {
+        // Creo un formulario mediante creaFormBuilder.
+        $form = $this->createFormBuilder(null)
+                    ->add('nombre',TextType::class, ['label'=>'Nombre'])
+                    ->add('imagen',TextType::class,['label'=>'Imagen'])
+                    ->add('descripcion',TextareaType::class,['label'=>'Descripción'])
+                    ->add('plataforma')
+                    ->add('genero')
+                    ->add('url',UrlType::class,['label'=>'Sitio web'])
+                    ->add('addGame',SubmitType::class)
+                    ->getForm();
+        return $this->render('form/bootstrap.html.twig', compact('form'));
     }
 }
