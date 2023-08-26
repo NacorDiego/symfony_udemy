4fd37d24787faa25ce9fe07630218416403ef18f
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-08-26 12:33:27 -0300

    Se agregan 'ChoiceType' a el campo 'Género' y 'Plataforma' para convertirlos en selects. Se agrega dropdown en la navbar para los formularios.

--- src/Form/GameFormType.php
+++ src/Form/GameFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\GameForm;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -18,8 +19,24 @@ class GameFormType extends AbstractType
             ->add('name',TextType::class, ['label'=>'Name'])
             ->add('image',TextType::class,['label'=>'Image'])
             ->add('description',TextareaType::class,['label'=>'Description'])
-            ->add('platform')
-            ->add('gender')
+            ->add('platform',ChoiceType::class,[
+                'choices'=>[
+                    'PC'=>1,
+                    'XBox'=>2,
+                    'PlayStation 4'=>3,
+                    'PlayStation 5'=>4,
+                    'Nintendo Switch'=>5
+                ],'placeholder'=>'Seleccionar...'
+            ]) // ChoiceType se utiliza para select y checkbox
+            ->add('gender',ChoiceType::class,[
+                'choices'=>[
+                    'Acción'=>1,
+                    'Aventura'=>2,
+                    'Rol'=>3,
+                    'Plataformas'=>4,
+                    'Terror'=>5
+                ],'placeholder'=>'Seleccionar...'
+            ])
             ->add('url',UrlType::class,['label'=>'Sitio web'])
             ;
     }
